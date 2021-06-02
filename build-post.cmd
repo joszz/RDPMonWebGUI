3150a2433cfaacc7532f9ba9ee2c07409f3db7ba
Jos Nienhuis <jos_nienhuis@hotmail.com>
2021-06-02 08:55:10 +0200

    Fixed minor styling problems

--- build-post.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if $(ConfigurationName) == Debug (
-	C:\Windows\System32\inetsrv\appcmd.exe start apppool /apppool.name:"RDPMon"
-)
-
-exit 0
--- build-pre.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-if $(ConfigurationName) == Debug (
-	C:\Windows\System32\inetsrv\appcmd.exe stop apppool /apppool.name:"RDPMon"
-)
-
-exit 0
